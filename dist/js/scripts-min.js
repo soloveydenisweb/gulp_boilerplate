3eb5339362613d25fae379724f05af8db920601a
soloveydenisweb <solovey_denis@outlook.com>
2019-06-21 13:57:10 +0300

    Удалил папку dist из удаленного репозитория

--- dist/js/scripts-min.js
+++
@@ -1,2 +0,0 @@
-function showMessage(){console.log("Привет всем присутствующим!")}function showMessage(){console.log("Привет, я - Вася!")}showMessage(),showMessage();
-//# sourceMappingURL=scripts-min.js.map
